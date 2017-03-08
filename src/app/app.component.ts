35323a932e2f80e6b3cea65ae042a86de675048d
HO KIM <HO KIM>
2017-03-08 23:24:21 +0900

    3/8_dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <a routerLink="/heroes">Heroes</a>
+        <router-outlet></router-outlet>
+    `
 })
+
 export class AppComponent {
-  title = 'app works!';
+    title = 'Tour of Heroes';
 }
